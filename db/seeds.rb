0bde4b6a842b6e25780c9822bad2610af2696835
Mike Mullin <mullin7@gmail.com>
2019-02-27 12:02:05 -0800

    mm - AddImageSeeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+IMAGE_URLS = [{ url: 'https://i.imgur.com/4W9Dlt0.jpg' },
+              { url: 'https://i.imgur.com/2oNdBlJ.jpg' },
+              { url: 'https://i.imgur.com/CQkK4Tq.jpg' },
+              { url: 'https://i.imgur.com/HHvtynI.jpg' },
+              { url: 'https://i.imgur.com/AwuVL0H.jpg' },
+              { url: 'https://i.imgur.com/LKmzukd.jpg' },
+              { url: 'https://i.imgur.com/ux2auwK.jpg' },
+              { url: 'https://i.imgur.com/qBa5ugK.jpg' },
+              { url: 'https://i.imgur.com/2uIeZ1v.jpg' },
+              { url: 'https://i.imgur.com/Ij0CCTy.jpg' },
+              { url: 'https://i.imgur.com/wkwaqbO.jpg' },
+              { url: 'https://i.imgur.com/VXpORkQ.jpg' },
+              { url: 'https://i.imgur.com/s4eLaRd.jpg' },
+              { url: 'https://i.imgur.com/UNn4Vg0.jpg' },
+              { url: 'https://i.imgur.com/cOR0sbU.jpg' },
+              { url: 'https://i.imgur.com/VhfjnsZ.jpg' },
+              { url: 'https://i.imgur.com/lnma4CB.jpg' },
+              { url: 'https://i.imgur.com/t1nyF57.jpg' }].freeze
+
+def init_images_table
+  Image.create!(IMAGE_URLS)
+end
+
+init_images_table
